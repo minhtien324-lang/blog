ff50cfe6e5add9c3f122cace4fd1ff25e494dcdb
mtien <19020169@vnu.edu.vn>
2024-09-04 15:31:25 +0700

    Update index.tsx

--- src/screens/contact/index.tsx
+++ src/screens/contact/index.tsx
@@ -5,20 +5,19 @@ const Contact = () => {
   return (
     <div>
       <ul className="social-links">
-        <li className="facebook">
-          <Link href="https://www.facebook.com/hngngn.10">Facebook</Link>
+        <li className="Facebook">
+          <Link href="https://www.facebook.com/TmT.Ls">Facebook</Link>
         </li>
-        <li className="twitter">
-          <Link href="https://twitter.com/marcus_killian1">Twitter</Link>
+        <li className="Instagram">
+          <Link href="https://www.instagram.com/mnhtn_iie__">Instagram</Link>
         </li>
-        <li className="youtube">
+        <li className="Youtube">
           <Link href="https://www.youtube.com/@hgtn3581">YouTube</Link>
         </li>
         <li className="gmail">
-          {/* mailto: <Link href={"https://accounts.google.com/Service}Login?hl=vi">ng.hoangtu.main@gmail.com</Link> */}
-          mailto:{" "}
-          <Link href="https://mail.google.com/mail/#inbox?compose=new&view=cm&fs=1&to=ng.hoangtu.main@gmail.com">
-            ng.hoangtu.main@gmail.com
+                    Mail to:{" "}
+          <Link href="https://mail.google.com/mail/#inbox?compose=new&view=cm&fs=1&to=minhtienuetls@gmail.com">
+            minhtienuetls@gmail.com
           </Link>
         </li>
       </ul>
